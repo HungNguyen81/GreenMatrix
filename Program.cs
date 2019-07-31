a8ae1fceab8fb282b3d5d6370bc9c2ee0a24df26
HungHois <50419437+HungNguyen81@users.noreply.github.com>
2019-07-31 17:11:39 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Threading;
 
+//nhược điểm: khó chỉnh sửa dòng chữ đỏ được in đè lên ma trận xanh
+
 public class RanMtr
 {
     static void Main()
@@ -14,16 +16,18 @@ public class RanMtr
         while (loop < 7)
         {
             count = 0;
-            while (count < 30)
+            while (count < 30) // in ra 30 dòng ngẫu nhiên
             {
                 for (int i = 0; i < 120; i++)
                 {
-                    Console.Write(ran.Next(10));
+                    Console.Write(ran.Next(10)); // trên mỗi dòng in ra 120 kí tự số ngẫu nhiên
                 }
                 Console.Write("\n");
-                Thread.Sleep(100);
+                Thread.Sleep(100); // dừng lại 0.1s rồi thực hiện tiếp câu lệnh sau
                 count++;
             }
+            // đưa con trỏ về vị trí thích hợp để in ra chữ H màu đỏ
+            #region Red_H
             Console.ForegroundColor = ConsoleColor.Red;
             for (int i = 12; i < 20; i++)
             {
@@ -40,16 +44,19 @@ public class RanMtr
                 Console.SetCursorPosition(58, i + line);
                 Console.Write("0101");
             }
+            #endregion
+            //đưa con trỏ chuột về vị trí tiếp tục tạo số ngẫu nhiên màu xanh 
             Console.ForegroundColor = ConsoleColor.Green;
-            Console.SetCursorPosition(0, 30 - 8 + line);
+            Console.SetCursorPosition(0, 30 - 8 + line); //ban đầu đoạn code trên được viết để hiện chữ H 
+                                                         //ở khoảng chính giữa màn hình Console, khởi tạo line = 8 để đẩy 
+                                                         //chữ H xuống phía dưới
             line += 30;
-            loop++;
-            //Console.ReadKey();
+            loop++; 
         }
-        while (true)
+        while (true) //vòng lặp vô hạn in ra đoạn chữ đỏ HOIS
         {
             count = 0;
-            while (count < 30)
+            while (count < 30) //in ra 30 dòng số ngẫu nhiên rồi tiếp tục in chữ đỏ
             {
                 for (int i = 0; i < 120; i++)
                 {
@@ -60,18 +67,17 @@ public class RanMtr
 
                 count++;
             }
+            #region Red_HOIS
             Console.ForegroundColor = ConsoleColor.Red;
             for (int i = 12; i < 20; i++)
             {
                 Console.SetCursorPosition(43, i + line);
                 Console.Write("010");
-
             }
             for (int i = 12; i < 20; i++)
             {
                 Console.SetCursorPosition(49, i + line);
                 Console.Write("010");
-
             }
             for (int i = 15; i < 17; i++)
             {
@@ -109,7 +115,7 @@ public class RanMtr
                 Console.SetCursorPosition(74, i + line);
                 Console.Write("01");
             }
-
+            
             Console.SetCursorPosition(70, 12 + line);
             Console.Write("010101");
             Console.SetCursorPosition(68, 19 + line);
@@ -120,11 +126,10 @@ public class RanMtr
                 Console.SetCursorPosition(70, i + line);
                 Console.Write("0101");
             }
+            #endregion
             Console.ForegroundColor = ConsoleColor.Green;
             Console.SetCursorPosition(0, 30 - 8 + line);
             line += 30;
-            //Console.ReadKey();
         }
     }
-
 }
